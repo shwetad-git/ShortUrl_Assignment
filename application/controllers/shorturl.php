9e86b4e5d95f426c0b993d4ccb0402f5ffb7a158
shwetad-git <45632662+shwetad-git@users.noreply.github.com>
2018-12-06 14:50:09 +0530

    Add functions for short url in controller

--- application/controllers/shorturl.php
+++ application/controllers/shorturl.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Shorten extends CI_Controller {
+class Shorturl extends CI_Controller {
 	/*
      * ======================================================================================================
      * Shorten Controller :
