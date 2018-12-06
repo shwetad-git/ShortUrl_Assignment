91848a186d7df5a39e4661582d18b3cfa299feb8
shwetad-git <45632662+shwetad-git@users.noreply.github.com>
2018-12-06 15:42:40 +0530

    Add short url form

--- application/views/display_short_url.php
+++ application/views/display_short_url.php
@@ -1,35 +1,13 @@
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-   <!--<![endif]-->
-   <head>
-      <meta charset="utf-8">
-      <!--[if IE]><![endif]-->
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-      <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-      <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-      <script type="text/javascript" src="<?php echo base_url();?>assets/js/common.js"></script>
-      <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
-      <title></title>
-      <meta name="description" content="">
-      <meta name="keywords" content="" />
-      <meta name="author" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- !CSS -->
-   </head>
-   <!-- !Body -->
-   <body>
+ <body>
       <div class="container">
          <section id="main">
             <h3 class="page-head"> URL SHORTNER </h3>
             <form action="/shorturl/" method="post" accept-charset="utf-8">
                <div class="wrap">
-                  <div class="search">
-                     <input type="text" name="url" class="searchTerm" placeholder="Enter URL to shorten it !">
-                     <button type="submit" class="searchButton">
-                     Shorten
-                     </button>
-                  </div>
+                   <div class="search">
+						<input type="text" name="url" class="searchTerm" placeholder="Enter URL to shorten it !">
+						<button type="submit" class="searchButton">MAKE SHORT URL</button>
+                   </div>
                </div>
             </form>
             <div class="shorturls">
@@ -86,4 +64,3 @@
       </div>
       <!--!/#container -->
    </body>
-</html>
