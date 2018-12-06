e78aaace5c0bd9aec7748ed11b525d981a069c63
shwetad-git <45632662+shwetad-git@users.noreply.github.com>
2018-12-06 15:41:55 +0530

    Add header footer files

---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+      <meta charset="utf-8">
+      <!--[if IE]><![endif]-->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+      <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+      <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+      <script type="text/javascript" src="<?php echo base_url();?>assets/js/common.js"></script>
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css" />
+      <title></title>
+      <meta name="description" content="">
+      <meta name="keywords" content="" />
+      <meta name="author" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- !CSS -->
+</head>
+<header class="page-header">
+  <h1>Example Page Header</h1>
+</header>
