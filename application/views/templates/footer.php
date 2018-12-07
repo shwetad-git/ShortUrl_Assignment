cc0e78cdd6ddc9e3ee9df16171b1d8028e2158ab
shwetad-git <45632662+shwetad-git@users.noreply.github.com>
2018-12-07 22:51:16 +0530

    Update footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,4 @@
 <footer class="container-fluid bg-4 text-center">
-  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p>
+  <p>Assignment Made By <a href='#'>Shweta Deshmukh</a></p>
 </footer> 
 </html>
