28ea31434cc567ef4107ce6e2eeb1f5b26a6fe31
shwetad-git <45632662+shwetad-git@users.noreply.github.com>
2018-12-08 23:18:03 +0530

    Set default controller

--- application/config/config.php
+++ application/config/config.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
-| Base Site URL
+| Base Site URL 
 |--------------------------------------------------------------------------
 |
 | URL to your CodeIgniter root. Typically this will be your base URL,
